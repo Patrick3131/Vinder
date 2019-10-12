77bba2ea0c779fa13ad76e9fdfc888cdb8f712d6
Patrick Fischer <patrick-fischer@live.de>
2019-10-12 08:33:40 +0100

    initial commit

---
+++ Vinder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vinder
+//
+//  Created by Patrick Fischer on 12.10.19.
+//  Copyright © 2019 Patrick Fischer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
