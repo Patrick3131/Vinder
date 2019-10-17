73e4aa0796980e2c8d25f46fa6484862b069db8e
Patrick Fischer <patrick-fischer@live.de>
2019-10-17 21:23:11 +0100

    add loginview

---
+++ Vinder/Views/Components/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  Vinder
+//
+//  Created by Patrick Fischer on 17.10.19.
+//  Copyright © 2019 Patrick Fischer. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        FUIAuthBaseViewControllerWrapper()
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
