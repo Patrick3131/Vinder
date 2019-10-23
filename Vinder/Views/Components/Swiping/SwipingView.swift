2b3c9a6a981b7880a1c0e3175bbc604b0ee651a4
Patrick Fischer <patrick-fischer@live.de>
2019-10-23 19:13:52 +0100

    Add Swiping

--- Vinder/Views/Components/Swiping/SwipingView.swift
+++ Vinder/Views/Components/Swiping/SwipingView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct SwipingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+             Text("Text")
+            CardView()
+        }
+       
     }
 }
 
---
+++ Vinder/Views/Components/Swiping/SwipingViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SwipingViewModel.swift
+//  Vinder
+//
+//  Created by Patrick Fischer on 22.10.19.
+//  Copyright © 2019 Patrick Fischer. All rights reserved.
+//
+
+import Foundation
