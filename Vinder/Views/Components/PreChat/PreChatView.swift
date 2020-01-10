a4e8ada38e34252b4a1f43ffe8ee5c37689a5437
Patrick Fischer <patrick-fischer@live.de>
2020-01-10 08:07:44 -0300

    add PreChat to project

---
+++ Vinder/Views/Components/PreChat/PreChatView.swift
@@ -0,0 +1,27 @@
+//
+//  PreChatView.swift
+//  Vinder
+//
+//  Created by Patrick Fischer on 10.01.20.
+//  Copyright © 2020 Patrick Fischer. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct PreChatView: View {
+    
+    var body: some View {
+        Text("")
+    }
+    
+}
+
+
+
+struct PreChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        PreChatView()
+    }
+}
---
+++ Vinder/Views/Components/PreChat/ProfileCountdownView.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileCountdownView.swift
+//  Vinder
+//
+//  Created by Patrick Fischer on 10.01.20.
+//  Copyright © 2020 Patrick Fischer. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct ProfileCountdownView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+
+
+struct ProfileCountdownView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileCountdownView()
+    }
+}
+
