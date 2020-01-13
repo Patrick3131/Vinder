1d4f13256d84b686f6e9aacb5f948d61f2b6ab0e
Patrick Fischer <patrick-fischer@live.de>
2020-01-13 17:42:22 -0300

    ProfileCountDownView added, shows how many hours are left for initial contact

--- Vinder/Views/Components/PreChat/ProfileCountdownView.swift
+++ Vinder/Views/Components/PreChat/ProfileCountdownView.swift
@@ -11,16 +11,24 @@ import SwiftUI
 
 
 struct ProfileCountdownView: View {
+    
+    var percentageFull: Double
+    var lineWidth:CGFloat = 10
+    var pictureUrl: URL
     var body: some View {
-        Text("")
+        ZStack {
+            CircleProfileView(pictureUrl: pictureUrl)
+                .padding(lineWidth)
+            OpenCircle(percentageFull: percentageFull, lineWidth: lineWidth)
+        }
     }
 }
 
-
-
 struct ProfileCountdownView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileCountdownView()
+        let string = "https://firebasestorage.googleapis.com/v0/b/vinder-cb83a.appspot.com/o/pictures%2Funnamed.jpg?alt=media&token=4742954b-3820-453c-be73-ba01f1b9fe5e"
+        let url = URL(string: string)
+        return ProfileCountdownView(percentageFull: 30, lineWidth: 10, pictureUrl: url!).frame(width: 200, height: 200, alignment: .center)
     }
 }
 
