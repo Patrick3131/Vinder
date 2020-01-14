220c49f302daf378aff1bb19fdb64d8000e39511
Patrick Fischer <patrick-fischer@live.de>
2020-01-14 12:04:56 -0300

    ProfileCountDownHoursView added to the project

---
+++ Vinder/Views/Components/PreChat/ProfileCountDownHoursView.swift
@@ -0,0 +1,39 @@
+//
+//  ProfileCountDownHoursView.swift
+//  Vinder
+//
+//  Created by Patrick Fischer on 13.01.20.
+//  Copyright © 2020 Patrick Fischer. All rights reserved.
+//
+
+import SwiftUI
+
+
+
+
+struct ProfileCountDownHoursView: View {
+    var viewModel: ProfileCountDownHoursViewModel
+    var lineWidth:CGFloat = 10
+
+    var body: some View {
+        VStack {
+            ProfileCountdownView(percentageFull: viewModel.percentageFull, lineWidth: lineWidth, pictureUrl: viewModel.pictureUrl)
+                .frame(width: 200, height: 200, alignment: .center)
+            Text(viewModel.hours)
+                .frame(width: 60, height: 20, alignment: .center)
+                .padding(.horizontal)
+                .background(Color.gray)
+            .cornerRadius(25)
+            
+        }
+    }
+}
+
+struct ProfileCountDownHoursView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["en", "de"], id: \.self) { id in
+            ProfileCountDownHoursView(viewModel: ProfileCountDownHoursViewModel()).frame(width: 200, height: 200, alignment: .center)
+                .environment(\.locale, .init(identifier: id))
+        }
+    }
+}
