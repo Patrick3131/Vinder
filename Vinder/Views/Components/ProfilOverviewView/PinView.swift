8814a2aa71c4217486f4d27882d035ea08999333
Patrick Fischer <patrick-fischer@live.de>
2020-01-24 18:33:31 -0300

    add PinView

---
+++ Vinder/Views/Components/ProfilOverviewView/PinView.swift
@@ -0,0 +1,34 @@
+//
+//  PinView.swift
+//  Vinder
+//
+//  Created by Patrick Fischer on 24.01.20.
+//  Copyright © 2020 Patrick Fischer. All rights reserved.
+//
+
+import SwiftUI
+
+import UIKit
+struct PinView: View {
+    
+    func createPinImage() -> UIImage {
+    
+        return (UIImage(named: "location-pin")?.withTintColor(.blue, renderingMode: .alwaysTemplate))!
+    }
+    
+    var body: some View {
+       ZStack {
+            Circle()
+                .fill(Color.lightBackground)
+            Image(uiImage: createPinImage())
+        .resizable()
+
+        }
+    }
+}
+
+struct PinView_Previews: PreviewProvider {
+    static var previews: some View {
+        PinView()
+    }
+}
